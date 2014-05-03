dacf243652a8c612b8a02764b5de689b806e07ce
David Finder <davidjfinder@gmail.com>
2014-05-03 13:06:18 -0400

    Renaming Query to Queries

---
+++ app/Controller/QueriesController.php
@@ -0,0 +1,11 @@
+<?php
+App::uses('AppController', 'Controller');
+
+class QueriesController extends AppController{
+	public function beforeFilter{
+		parent::beforeFilter();
+	}
+	public function index(){
+		$queries=$this->
+	}
+}
